443df1d9c97f3dc8d3563c37d19e3866454a6491
acsanti92 <armando.santiago@pluto.tv>
2023-02-27 11:40:06 -0800

    QR Generation function set up, w dwnld capability

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { QRCode } from "qrcode"
+import QRCode from "qrcode"
 import { useState } from "react"
 
 function App() {
   const [url, seturl] = useState("")
-  const [qrCode, setqrCode] = useState("")
+  const [qr, setqrCode] = useState("")
+
+  const generateQRCode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) return console.error(err)
+      setqrCode(url)
+    })
+  }
 
   return (
     <div className="app">
       <h1>Armando's QR Code Generator</h1>
       <input 
         type="text"
-        placeholder="e.g. https://google.com" />
-        <button>Generate</button>
-        <img src="" alt="" />
+        placeholder="e.g. https://google.com"
+        value={url}
+        onChange={(evt) => seturl(evt.target.value)} />
+      <button onClick={generateQRCode}>Generate</button>
+      {qr && <>
+        <img src={qr} />
+        <a href={qr} download="qrcode.png">Download</a>
+      </>}
     </div>
   )
 }
