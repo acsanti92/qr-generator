6223a2fa525fa0e9f13be747c4be973122157bd9
acsanti92 <armando.santiago@pluto.tv>
2023-02-27 11:12:22 -0800

    Import QRCode and useState Dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { QRCode } from "qrcode"
+import { useState } from "react"
+
 function App() {
+  const [url, seturl] = useState("")
+  const [qrCode, setqrCode] = useState("")
 
   return (
     <div className="app">
-      <h1>Armando QR</h1>
+      <h1>Armando's QR Code Generator</h1>
+      <input 
+        type="text"
+        placeholder="e.g. https://google.com" />
+        <button>Generate</button>
+        <img src="" alt="" />
     </div>
   )
 }
